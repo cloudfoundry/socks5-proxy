d215ff3f7fb9d4274a780444658fd6608c3be6ce
Joseph Palermo <jpalermo@pivotal.io>
2025-04-07 11:41:13 -0700

    Move pipeline to fiwg concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,5 +4,5 @@ set -eu
 
 FLY="${FLY_CLI:-fly}"
 
-${FLY} -t "${CONCOURSE_TARGET:-bosh-ecosystem}" set-pipeline -p "socks5-proxy" \
+${FLY} -t "${CONCOURSE_TARGET:-bosh}" set-pipeline -p "socks5-proxy" \
     -c ci/pipeline.yml
